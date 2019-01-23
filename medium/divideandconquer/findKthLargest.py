6cd0243c99e835fdbcc6008dad82010e7b241120
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2019-01-23 00:22:31 -0500

    Create findKthLargest.py

---
+++ medium/divideandconquer/findKthLargest.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        
+        
+        def quickselect(arr,val):
+
+            pi=random.randint(0, len(arr)-1)
+            piv= arr[pi]
+            
+            s =  []
+            b =  []
+        
+         
+            for i,x in enumerate(arr):
+                if i==pi:
+                    continue
+                if x >piv:
+                    b.append(x)
+                elif x<=piv:
+                    s.append(x)
+
+
+            b.append(piv)
+            
+            if len(b)==val:
+                return piv
+            elif len(b)>val:
+                return quickselect(b,val)
+            elif len(b)<val:
+                return quickselect(s,val-len(b))
+            
+        
+        return quickselect(nums,k)
