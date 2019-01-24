49ae4c9a021b637a2e2b15f7cace3c511a816a7e
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2019-01-23 22:31:22 -0500

    Update permute.py

--- medium/backtracking/permute.py
+++ medium/backtracking/permute.py
@@ -1,3 +1,23 @@
+"""
+#46 Permutations
+
+Given a list, generate all permutation.
+
+Algorithm for function Permute(list l)
+
+1. create an empty stack s.
+2. for every element i in l:
+    2.1 push i to stack
+    2.2 remove i from l
+    2.3 call Permute(l,p)
+    2.4 pop i from stack
+    2.5 if length of stack equal to l, add it to result
+
+Runtime : O(N!)
+
+
+"""
+
 from collections import deque
 
 class Solution:
