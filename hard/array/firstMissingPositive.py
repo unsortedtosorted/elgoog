259b23ec303384a814136b3aaa5fdf5701ff06fb
unsortedtosorted <44731492+unsortedtosorted@users.noreply.github.com>
2019-01-28 23:49:49 -0500

    Create firstMissingPositive.py

---
+++ hard/array/firstMissingPositive.py
@@ -0,0 +1,34 @@
+"""
+41. First Missing Positive
+
+Runtime : O(N)
+
+"""
+
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+      
+        if not nums:
+            return 1
+            
+        for i, num in enumerate(nums):
+            while i + 1 != nums[i] and 0 < nums[i] <= len(nums):
+                
+                v = nums[i]
+                nums[i] = nums[v - 1]
+                nums[v - 1] = v
+                
+                if nums[i] == nums[v - 1]:
+                    break
+        
+        for i, num in enumerate(nums, 1):
+            if num != i:
+                return i
+        
+        return len(nums) + 1
